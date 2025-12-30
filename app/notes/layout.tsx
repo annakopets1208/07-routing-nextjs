a0509476d38fa2c8764f4d06a9d10a43ded4f251
Anna Kopets <akopc2012@gmail.com>
2025-12-30 15:41:39 +0200

    all

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -7,10 +7,8 @@ import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
 export default async function NotesLayout({
-  sidebar,
   children,
 }: {
-  sidebar: React.ReactNode;
   children: React.ReactNode;
 }) {
   const queryClient = new QueryClient();
@@ -23,7 +21,6 @@ export default async function NotesLayout({
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NotesClient />
-      {sidebar}
       {children}
     </HydrationBoundary>
   );
